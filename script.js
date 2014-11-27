f81105471e54b21fa4ea498c1f37fc3d8d05dc95
Cucazer <cucazik@inbox.lv>
2014-11-27 09:48:45 +0100

    Begin to implement EaselJS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas,context;
+var canvas,context,stage;
 var stopGame=false;
 
 // A cross-browser requestAnimationFrame
@@ -14,26 +14,48 @@ var requestAnimFrame = (function(){
 		};
 	})();
 
+/*
+var circle = new createjs.Shape();
+circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+circle.x = 100;
+circle.y = 100;
+stage.addChild(circle);
+*/
 var wall = function(x){
-	this.x = x;
 	this.width = 50;
 	this.gapWidth = 100;
 	this.upperBorder = Math.floor(Math.random()*375) + 25;
 	this.lowerBorder = this.upperBorder + this.gapWidth;
 	this.lowerHeight = 500 - this.lowerBorder;
 
+	//CreateJS visualisation
+	this.shapeUpper = new createjs.Shape();
+	this.shapeUpper.x = x;
+	this.shapeUpper.y = 0;
+	this.shapeUpper.graphics.beginFill("black").drawRect(0,0,this.width,this.upperBorder);
+	stage.addChild(this.shapeUpper);
+	this.shapeLower = new createjs.Shape();
+	this.shapeLower.x = x;
+	this.shapeLower.y = this.lowerBorder;
+	this.shapeLower.graphics.beginFill("black").drawRect(0,0,this.width,this.lowerHeight);
+	stage.addChild(this.shapeLower);
+
 	this.renew = function(){
-		this.x += 1250;
+		this.shapeUpper.x += 1250;
+		this.shapeLower.x += 1250;
 		this.upperBorder = Math.floor(Math.random()*375) + 25;
 		this.lowerBorder = this.upperBorder + this.gapWidth;
 		this.lowerHeight = 500 - this.lowerBorder;
+		this.shapeUpper.graphics.beginFill("black").drawRect(0,0,this.width,this.upperBorder);
+		this.shapeLower.graphics.beginFill("black").drawRect(0,0,this.width,this.lowerHeight);
 		step = step * 1.2;
 		score++;
 		};
 
 	this.move = function(step){
-		this.x -= step;
-		if (this.x <= -this.width){
+		this.shapeUpper.x -= step;
+		this.shapeLower.x -= step;
+		if (this.shapeUpper.x <= -this.width){
 			this.renew();
 		};
 	}
@@ -49,22 +71,34 @@ function collision(me,obj){
 
 //Array of scene objects
 var objects = new Array();
+
+
+
+
+//here to createjs__
+
+
+
 var step, score;
 function init() {
 	step = 2;
 	score = 0;
 	//Creating main character
 	objects[0] = {
-		x : 25,
-		y : 225,
-		height: 50,
-		width: 50,
-		move : function(step){
-			if (this.y+step<=450 && this.y+step>=0){
-				this.y += step;
+		width : 50,
+		height : 50
+	};
+	objects[0].shape = new createjs.Shape();
+	objects[0].shape.x = 25;
+	objects[0].shape.y = 225;
+	objects[0].move = function(step){
+			if (this.shape.y+step<=450 && this.shape.y+step>=0){
+				this.shape.y += step;
 			};
 		}
-	};
+	objects[0].shape.graphics.beginFill("red").drawRect(0,0,50,50);
+	stage.addChild(objects[0].shape);
+
 	//Creating walls
 	for (i=1;i<=5;i++){
 		objects[i] = new wall(i*250);
@@ -89,15 +123,17 @@ function drawObjects(c){
 	c.fillRect(0,0,800,500);
 
 	//Main character
-	c.fillStyle = "#FF0000";
+	/*c.fillStyle = "#FF0000";
 	c.fillRect(objects[0].x,objects[0].y,objects[0].width,objects[0].height);
-
+*/
 	//Walls
+	/*
 	c.fillStyle = "#000000";
 	for (i=1;i<=5;i++){
 		c.fillRect(objects[i].x,0,objects[i].width,objects[i].upperBorder);
 		c.fillRect(objects[i].x,objects[i].lowerBorder,objects[i].width,objects[i].lowerHeight);
-	}
+	}*/
+	stage.update();
 
 	//Score
 	c.fillStyle = "#0000FF";
@@ -115,6 +151,14 @@ function mainLoop(){
 
 	updateScene(dt);
 	drawObjects(context);
+/*
+	var circle = new createjs.Shape();
+	circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);*/
+//	stage.update();
+
 	for (i=0;i<=5;i++){
 		if (collision(objects[0],objects[i])) {
 			context.fillStyle = "#FF0000";
@@ -141,8 +185,10 @@ addEventListener("keyup", function (e) {
 }, false);
 
 window.onload = function(){
-canvas = document.getElementById('canvas');
+canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
+//CreateJS
+stage = new createjs.Stage("canvas");
 document.getElementById("stop").onclick = function(){stopGame = true};
 document.getElementById("again").onclick = function(){
 	stopGame = true;
